5fb58bcfaea81276d83072806cd1df1b6925a5da
Bilal Ahmad Shaheen <bilaiahmad.shaheen@gmail.com>
2026-07-15 23:10:56 +0500

    fixed base url dynamically

--- src/store/services/aapApi.js
+++ src/store/services/aapApi.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const appApi = createApi({
